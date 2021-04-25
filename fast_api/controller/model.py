7e6942d531e1e8d2a3cd8b8e928fb1d68dd1ea21
Sonam Ramchand <sonam.ramchand@venturedive.com>
2021-04-25 01:43:24 -0700

    fast api with real model

--- fast_api/controller/model.py
+++ fast_api/controller/model.py
@@ -13,10 +13,9 @@ with open("config.json") as json_file:
 class Model:
     def __init__(self):
         self.device = "cpu"
-        default_model_config = AutoConfig.from_pretrained(config["DEFAULT_MODEL_NAME"], num_labels=27)
+        default_model_config = AutoConfig.from_pretrained(config["DEFAULT_MODEL_NAME"], num_labels=2)
 
         default_model = AutoModelForSequenceClassification.from_config(config=default_model_config)
-        pos_neg_model = AutoModelForSequenceClassification.from_config(config=default_model_config)
         
         self.default_tokenizer = AutoTokenizer.from_pretrained(config["DEFAULT_MODEL_NAME"], use_fast=True)
         
@@ -26,33 +25,16 @@ class Model:
         default_model = default_model.eval()
         self.default_model = default_model.to(self.device)
 
-        pos_neg_model.load_state_dict(
-            torch.load(config["POS_NEG_MODEL"], map_location=self.device)
-        )
-        pos_neg_model = pos_neg_model.eval()
-        self.pos_neg_model = pos_neg_model.to(self.device)
-
-    def detect_emotion_full(self, text):        
+    def multilingual_news_detect(self, text):        
         prepared_input = self.default_tokenizer.prepare_seq2seq_batch([text], return_tensors='pt')
         self.default_model = self.default_model.to('cpu')
         self.default_model.eval()
         model_output = self.default_model(**prepared_input)
         prediction = np.argmax(model_output.logits[0].detach().numpy())
-
+        print(prediction)
+        
         return (
             config["DEFAULT_CLASS_NAMES"][prediction]
-        )
-
-    def detect_emotion_binary(self, text):        
-        prepared_input = self.default_tokenizer.prepare_seq2seq_batch([text], return_tensors='pt')
-        self.pos_neg_model = self.pos_neg_model.to('cpu')
-        self.pos_neg_model.eval()
-        model_output = self.pos_neg_model(**prepared_input)
-        print(model_output)
-        prediction = np.argmax(model_output.logits[0].detach().numpy())
-
-        return (
-            config["POS_NEG_CLASS_NAMES"][prediction]
         )
 
 
@@ -61,3 +43,4 @@ model = Model()
 
 def get_model():
     return model
+
--- fast_api/routes/api.py
+++ fast_api/routes/api.py
@@ -6,24 +6,17 @@ from controller.model import Model, get_model
 app = FastAPI()
 
 
-class SentimentRequest(BaseModel):
+class NewsRequest(BaseModel):
     text: str
 
 
-class SentimentResponse(BaseModel):
+class NewsResponse(BaseModel):
     sentiment: str
     
 
-@app.post("/detect_emotion_full", response_model=SentimentResponse)
-def detect_emotion_full(request: SentimentRequest, model: Model = Depends(get_model)):
-    sentiment = model.detect_emotion_full(request.text)
-    return SentimentResponse(
-        sentiment=sentiment
-    )
-
-@app.post("/detect_emotion_binary", response_model=SentimentResponse)
-def detect_emotion_binary(request: SentimentRequest, model: Model = Depends(get_model)):
-    sentiment = model.detect_emotion_binary(request.text)
-    return SentimentResponse(
+@app.post("/multilingual_news_detect", response_model=NewsResponse)
+def multilingual_news_detect(request: NewsRequest, model: Model = Depends(get_model)):
+    sentiment = model.multilingual_news_detect(request.text)
+    return NewsResponse(
         sentiment=sentiment
     )
