42b3bb9af90e91675fa62e541495a2017d2b5417
abdulsamad <abdulsamad19951@gmail.com>
2021-05-02 23:59:34 +0500

    Completed phase 3 submission

--- fast_api/fastapi/routes/api.py
+++ fast_api/fastapi/routes/api.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD:fast_api/fastapi/routes/api.py
-from typing import Dict
-from fastapi import Depends, FastAPI
-from pydantic import BaseModel
-from controller.model import Model, get_model
-
-app = FastAPI()
-
-
-class NewsRequest(BaseModel):
-    text: str
-
-
-class NewsResponse(BaseModel):
-    sentiment: str
-    
-
-@app.post("/multilingual_news_detect", response_model=NewsResponse)
-def multilingual_news_detect(request: NewsRequest, model: Model = Depends(get_model)):
-    sentiment = model.multilingual_news_detect(request.text)
-    return NewsResponse(
-        sentiment=sentiment
-    )
-=======
 from typing import Dict
 from fastapi import Depends, FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
@@ -55,4 +31,4 @@ def multilingual_news_detect(request: Request, news: str = Form(...), model: Mod
     return NewsResponse(
         sentiment=sentiment
     )
->>>>>>> 48e297e96e6ffcfe14c5237666c3e3b7024e0892:fast_api/routes/api.py
+
